115c0add809df913021bcf1a3eb2211b0b740924
93parkkyungho <kh.park@alcherainc.com>
2024-01-11 07:27:10 +0900

    Revert "ewfwefewf"
    This reverts commit ec1f05311f18fe775a807f40fdd59b4661c1861a.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}122123 12</h1>
+    <h1>{{ msg }}122123 12123</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
